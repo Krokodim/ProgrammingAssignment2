bc35f0aa7c12017ff7dc21a4d9991c6471f1979e
Krokodim <dmitry.grekov@gmail.com>
2015-01-15 23:37:30 +0300

    Test function added

--- cachematrix.R
+++ cachematrix.R
@@ -24,23 +24,19 @@ makeCacheMatrix <- function(x = matrix()) {
   
   m <- NULL
   
-  set <- function (y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function (solve) m <<- solve
-  getsolve <- function () m
+  set <- function (y) { x <<- y; m <<- NULL }
+  get <- function()   { x }
+  setsolve <- function (solve) { m <<- solve }
+  getsolve <- function ()      { m }
+  
   list(get=get, 
        set=set, 
        getsolve=getsolve, 
        setsolve=setsolve)
-
 }
 
-
 ## cacheSolve 
-## ---------------
+## ----------
 ## Accepts the initial matrix in a form returned by 
 ## makeCacheMatrix function and returns an inversed
 ## matrix. If the matrix was already inverted, reads
@@ -56,4 +52,39 @@ cacheSolve <- function(x, ...) {
   m <- solve(data,...)
   x$setsolve(m)
   m
+}
+
+## testCacheSolve
+## --------------
+## Just a sample function showing how to use cacheSolve 
+## and makeCacheMatrix funcions. 
+##
+testCacheSolve <- function () {
+  #size for a test matrix
+  dim <-100
+  
+  #init a test matrix #1
+  m1 <- matrix(rnorm(dim * dim), dim)
+  
+  #init caching
+  mx1 <- makeCacheMatrix(m1)
+  
+  #calc the inverted matrix (the inversion is calculated)
+  mi11 <- cacheSolve(mx1)
+  
+  #do something ...
+  # ...
+  
+  #calc it again (now it is read from a cache)
+  mi12 <- cacheSolve(mx1)
+  
+  #set another matrix
+  m2 <- matrix(rnorm(dim * dim), dim)
+  mx2 <- makeCacheMatrix(m2)
+  
+  #calc it - it is calculated
+  mi21 <- cacheSolve(mx2)  
+  
+  #and now it is read from a cache
+  mi22 <- cacheSolve(mx2)  
 }
