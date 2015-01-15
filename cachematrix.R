c264852870cb5b9b2f36aaf0ab2884a41d9e1588
Krokodim <dmitry.grekov@gmail.com>
2015-01-15 23:06:05 +0300

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,29 @@
+###############################################################
+##                                                           ##
+##           cachematrix.R                  15.01.2015       ##
+##                                                           ##
+##-----------------------------------------------------------##
+##                                                           ##
+##    This module contains a set of functions to solve       ##
+##  square matricies with caching                            ##
+##                                                           ##
+###############################################################
 
+
+
+## makeCacheMatrix
+## ---------------
+## accepts  an inversible square matrix, stores it and 
+## returns an interface to manipulate the data:
+##    - set(x)          stores the initial matrix
+##    - get()           returns the initial matrix
+##    - setsolve(x)     stores the solved matrix
+##    - getsolve()      returns the solved matrix (or NULL)
+##
 makeCacheMatrix <- function(x = matrix()) {
+  
   m <- NULL
+  
   set <- function (y) {
     x <<- y
     m <<- NULL
@@ -8,11 +31,21 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setsolve <- function (solve) m <<- solve
   getsolve <- function () m
-  list(get=get, set=set, getsolve=getsolve, setsolve=setsolve)
+  list(get=get, 
+       set=set, 
+       getsolve=getsolve, 
+       setsolve=setsolve)
+
 }
 
 
-
+## cacheSolve 
+## ---------------
+## Accepts the initial matrix in a form returned by 
+## makeCacheMatrix function and returns an inversed
+## matrix. If the matrix was already inverted, reads
+## the result from the cache, without calculations
+## 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
   if(!is.null(m)) {
@@ -20,7 +53,7 @@ cacheSolve <- function(x, ...) {
     return(m)
   }
   data <- x$get()
-  m <- solve(data)
+  m <- solve(data,...)
   x$setsolve(m)
   m
 }
